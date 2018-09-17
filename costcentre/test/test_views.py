4787914dd7ac9ca58403f1cade3ff0974c858792
Luisella Strona <luisella21@hotmail.com>
2018-09-17 08:27:32 +0100

    upgraded to Django 2.1

--- costcentre/test/test_views.py
+++ costcentre/test/test_views.py
@@ -1,24 +1,3 @@
-# from django.test import RequestFactory
-#
-# #https://matthewdaly.co.uk/blog/2015/08/02/testing-django-views-in-isolation/
-# class SnippetCreateViewTest(TestCase):
-#     """
-#     Test the snippet create view
-#     """
-#     def setUp(self):
-#         self.user = UserFactory()
-#         self.factory = RequestFactory()
-#     def test_get(self):
-#         """
-#         Test GET requests
-#         """
-#         request = self.factory.get(reverse('snippet_create'))
-#         request.user = self.user
-#         response = SnippetCreateView.as_view()(request)
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(response.context_data['user'], self.user)
-#         self.assertEqual(response.context_data['request'], request)
-
 import pytest
 import unittest
 from django.urls import reverse
@@ -34,6 +13,7 @@ class SimpleTest(unittest.TestCase):
     def test_details(self):
         # Issue a GET request.
         response = self.client.get(reverse('costcentrefilter'))
+        # if redirecting, check that we are sending to a login page
         if response.status_code == 302:
             self.assertIn ('login', response.url)
         else:
