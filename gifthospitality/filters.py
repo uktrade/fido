81afff70b26be6289612b2e167bf0a06426ddc7f
Luisella Strona <luisella21@hotmail.com>
2018-12-18 09:48:04 +0000

    Use dropdown for filetrs, when appropriate

--- gifthospitality/filters.py
+++ gifthospitality/filters.py
@@ -1,19 +1,25 @@
-from  django_filters import DateFromToRangeFilter, NumberFilter, CharFilter
+from  django_filters import DateFromToRangeFilter, NumberFilter, CharFilter, ModelChoiceFilter
 
-from .models import GiftAndHospitality
+from .models import GiftAndHospitality, GiftAndHospitalityCategory, \
+    GiftAndHospitalityClassification, GiftAndHospitalityCompany
 
 from core.filters import MyFilterSet
-
+from payroll.models import Grade
 
 class GiftHospitalityFilter(MyFilterSet):
     entered_date_stamp = DateFromToRangeFilter()
     value = NumberFilter(lookup_expr='lte', label='Max value of offer (£)' )
-    # rep =  CharFilter(lookup_expr='icontains')
+    # use a dropdown to search the following fields
+    category =  ModelChoiceFilter(queryset=GiftAndHospitalityCategory.objects.all())
+    gift_type =  ModelChoiceFilter(queryset=GiftAndHospitalityClassification.objects.all())
+    company =  ModelChoiceFilter(queryset=GiftAndHospitalityCompany.objects.all())
+    grade = ModelChoiceFilter(queryset=Grade.objects.all())
+
     class Meta(MyFilterSet.Meta):
             model = GiftAndHospitality
             fields = [ 'id',
             'category',
-            'classification',
+            'gift_type',
             'value',
             'rep',
             'grade',
