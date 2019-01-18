d46963f0c6b678f012975f603f0a00ed558c5526
Luisella Strona <luisella21@hotmail.com>
2019-01-18 16:18:29 +0000

    Beeter interface for downloads

--- admintool_support/admin.py
+++ admintool_support/admin.py
@@ -3,12 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from django.urls import path
 
+from core.admin import AdminreadOnly
+
 from .exportcsv import export_cost_centres, export_nac_hierarchy
 
 from .models import DownloadLog
 
-class AdminToolExport(admin.ModelAdmin):
+class AdminToolExport(AdminreadOnly):
     change_list_template = "admin/admintool_changelist.html"
+    list_display = ('download_type', 'downloader', 'download_time')
+    # fields = ('download_type', 'downloader', 'download_time')
+    list_display_links = None
 
     def get_urls(self):
         urls = super().get_urls()
@@ -19,12 +24,15 @@ class AdminToolExport(admin.ModelAdmin):
         return my_urls + urls
 
     def export_cc(self, request):
+        #request.user
+        DownloadLog.objects.create(downloader=request.user,
+                                   download_type=DownloadLog.CC_AT)
         return export_cost_centres()
 
     def export_nac(self, request):
+        DownloadLog.objects.create(downloader=request.user,
+                                   download_type=DownloadLog.NAC_H_AT)
         return export_nac_hierarchy()
-
-
 
 
 admin.site.register(DownloadLog, AdminToolExport)
--- admintool_support/models.py
+++ admintool_support/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
-from core.metamodels import SimpleTimeStampedModel
-# Create your models here.
-class DownloadLog(SimpleTimeStampedModel):
-    download_type = models.CharField('Download Type', max_length=300)
+class DownloadLog(models.Model):
+    CC_AT = 'CC'
+    NAC_H_AT = 'NAC H'
+    DOWNLOAD_CHOICE = (
+        (CC_AT, 'Cost Centre Hierarchy for Admin Tool'),
+        (NAC_H_AT, 'NAC Hierarchy for Admin Tool')
+    )
+    download_type = models.CharField('Download Type', choices=DOWNLOAD_CHOICE, max_length=300)
     downloader = models.CharField('Download by', max_length=300)
+    download_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Download Log"
+        verbose_name_plural = "Download Logs"
+        ordering = ['-download_time']
