b67c17be88d68f496e529f0341241a2728690943
Ross Miller <ross@decodedcollective.com>
2019-09-09 18:02:42 +0100

    Fix hot reloading for Django rendered page

--- fido/front_end/fido/config-overrides.js
+++ fido/front_end/fido/config-overrides.js
@@ -1,35 +1,25 @@
 var BundleTracker = require('webpack-bundle-tracker');
+var webpack = require('webpack')
 
 module.exports = {
   webpack: (config, env) => {
-
-    config.plugins.push(
-      new BundleTracker({
+    config.plugins = [
+    new webpack.HotModuleReplacementPlugin(),
+    new BundleTracker({
         path: __dirname,
         filename: './config/webpack-stats.json',
       }),
-    );
-
-    // ' + paths.serverHostname + '
-
-    // config.entry = [
-    //     require.resolve('webpack-dev-server/client') + '?http://localhost:3000',
-    //     require.resolve('webpack/hot/dev-server'),
-    //   // Finally, this is your app's code:
-    //   //config.paths.appIndexJs,
-    // ]
+  ];
 
   config.entry = [
       'webpack-dev-server/client?http://localhost:3000',
-      'webpack/hot/only-dev-server',
-      '/fido/front_end/fido/src/index.js'
-  ],
+      'webpack/hot/dev-server',
+      '/fido/front_end/fido/src/index'
+  ];
 
     config.output = {
       path: '/fido/front_end/fido/build/static/',
-      //filename: "[name].[hash].js",
       publicPath: 'http://localhost:3000/',
-        //public: "http://localhost:3000"
     };
 
     config.optimization.splitChunks.name = 'vendors';
@@ -37,22 +27,16 @@ module.exports = {
     return config;
   },
   devServer: function(configFunction) {
-    // Return the replacement function for create-react-app to use to generate the Webpack
-    // Development Server config. "configFunction" is the function that would normally have
-    // been used to generate the Webpack Development server config - you can use it to create
-    // a starting configuration to then modify instead of having to create a config from scratch.
     return function(proxy, allowedHost) {
-      // Create the default config by calling configFunction with the proxy/allowedHost parameters
       const config = configFunction(proxy, allowedHost);
-
         config.headers = {
           'Access-Control-Allow-Origin': '*'
         }
         config.hot = true,
         config.public = "localhost:3000"
-
-      // Return your customised Webpack Development Server config.
       return config;
     };
   },
 };
+
+    // ' + paths.serverHostname + '
