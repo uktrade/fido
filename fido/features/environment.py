11d08fb5b12c6c13700f08ee1355f6578f1795cd
Ross Miller <ross@decodedcollective.com>
2019-09-26 09:52:38 +0100

    Add browserstack remote automation

--- fido/features/environment.py
+++ fido/features/environment.py
@@ -1,39 +1,62 @@
-from pyvirtualdisplay import Display
 from selenium import webdriver
-import sys
-from django.core.management import call_command
+from browserstack.local import Local
+from django.conf import settings
+import os, json
 
-from selenium.webdriver.chrome.options import Options
-chrome_options = Options()
+TASK_ID = int(os.environ['TASK_ID']) if 'TASK_ID' in os.environ else 0
+
+CONFIG = {
+  "server": "hub.browserstack.com",
+  "user": "uktitools1",
+  "key": "rJFxac2sLtJvBn5hUdMK",
+
+  "capabilities": {
+    "browserstack.local": True,
+    "name": "Bstack-[Behave] Local Test"
+  },
+
+  "environments": [{
+    "browser": "chrome"
+  }]
+}
+
+bs_local = None
+
+BROWSERSTACK_USERNAME = settings.BROWSERSTACK_USERNAME
+BROWSERSTACK_ACCESS_KEY = settings.BROWSERSTACK_ACCESS_KEY
+
+def start_local():
+    """Code to start browserstack local before start of test."""
+    global bs_local
+    bs_local = Local()
+    bs_local_args = { "key": BROWSERSTACK_ACCESS_KEY, "forcelocal": "true" }
+    bs_local.start(**bs_local_args)
+
+def stop_local():
+    """Code to stop browserstack local after end of test."""
+    global bs_local
+    if bs_local is not None:
+        bs_local.stop()
 
 
-def before_all(context):
-    # Add test data
-    # call_command('create_stub_data', 'All')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_experimental_option('useAutomationExtension', False)
+def before_feature(context, feature):
+    desired_capabilities = CONFIG['environments'][TASK_ID]
 
-    # capabilities = {
-    #     'browserName': 'chrome',
-    #     'chromeOptions': {
-    #         'useAutomationExtension': False,
-    #         'forceDevToolsScreenshot': True,
-    #         'args': ['--start-maximized', '--disable-infobars']
-    #     }
-    # }
+    for key in CONFIG["capabilities"]:
+        if key not in desired_capabilities:
+            desired_capabilities[key] = CONFIG["capabilities"][key]
 
-    display = Display(visible=0, size=(800, 600))
-    display.start()
+    if 'BROWSERSTACK_APP_ID' in os.environ:
+        desired_capabilities['app'] = os.environ['BROWSERSTACK_APP_ID']
 
-    # now Chrome will run in a virtual display.
-    # you will not see the browser.
-    context.driver = webdriver.Chrome(options=chrome_options)
-    context.driver.get('http://www.google.com')
-    print(context.driver.title, sys.stdout)
+    if "browserstack.local" in desired_capabilities and desired_capabilities["browserstack.local"]:
+        start_local()
 
+    context.browser = webdriver.Remote(
+        desired_capabilities=desired_capabilities,
+        command_executor="http://%s:%s@%s/wd/hub" % (BROWSERSTACK_USERNAME, BROWSERSTACK_ACCESS_KEY, CONFIG['server'])
+    )
 
-def after_all(context):
-    context.driver.close()
-    context.display.stop()
+def after_feature(context, feature):
+    context.browser.quit()
+    stop_local()
