6e809875b7143426e71a7941da2722bc36157d2a
Ross Miller <ross@decodedcollective.com>
2019-10-07 16:32:05 +0100

    Amend folder structure to use DIT conventions

--- fido/front_end/src/Components/RowContext/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const RowContext = React.createContext({})
-
-export const RowProvider = RowContext.Provider
-export const RowConsumer = RowContext.Consumer
-export default RowContext
