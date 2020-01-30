18400bbca574cbb73eb9cb10f3f92e419d55cd08
Luisella Strona <luisella21@hotmail.com>
2020-01-30 09:38:57 +0000

    Safety commit.

--- download_file/views/view_oscar_return.py
+++ download_file/views/view_oscar_return.py
@@ -1,15 +1,15 @@
 from django.views.generic.base import TemplateView
 
-from download_file.decorators import has_download_permission
+from download_file.decorators import has_download_oscar_permission
 from download_file.models import FileDownload
 
 
-class DownloadView(TemplateView):
+class DownloadOscarReturnView(TemplateView):
     template_name = "download_file/downloaded_files.html"
 
-    @has_download_permission
+    @has_download_oscar_permission
     def dispatch(self, request, *args, **kwargs):
-        return super(DownloadView, self).dispatch(request, *args, **kwargs)
+        return super(DownloadOscarReturnView, self).dispatch(request, *args, **kwargs)
 
     def downloaded_files(self):
         downloaded_files = FileDownload.objects.all().order_by(
