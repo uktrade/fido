40f1762ba2ccdc7b36282c993672d1e3a7e63787
Luisella Strona <luisella21@hotmail.com>
2020-03-24 15:50:04 +0000

    Test.

--- forecast/create_oscar_report.py
+++ forecast/create_oscar_report.py
@@ -29,33 +29,37 @@ def export_oscarreport_iterator(queryset):
         "MAR",
     ]
     for obj in queryset:
-        yield [
-            obj.row_number,
-            "UKT013",
-            "UK TRADE & INVESTMENT",
-            obj.account_l5_code.account_l5_code
-            if obj.account_l5_code
-            else "",
-            obj.account_l5_code.account_l5_long_name
-            if obj.account_l5_code
-            else "",
-            obj.sub_segment_code,
-            obj.sub_segment_long_name,
-            "TYPE_INYEAR",
-            "IN-YEAR RETURN",
-            obj.apr,
-            obj.may,
-            obj.jun,
-            obj.jul,
-            obj.aug,
-            obj.sep,
-            obj.oct,
-            obj.nov,
-            obj.dec,
-            obj.jan,
-            obj.feb,
-            obj.mar,
-        ]
+        try:
+            yield [
+                obj.row_number,
+                "UKT013",
+                "UK TRADE & INVESTMENT",
+                obj.account_l5_code.account_l5_code
+                if obj.account_l5_code
+                else "",
+                obj.account_l5_code.account_l5_long_name
+                if obj.account_l5_code
+                else "",
+                obj.sub_segment_code,
+                obj.sub_segment_long_name,
+                "TYPE_INYEAR",
+                "IN-YEAR RETURN",
+                obj.apr,
+                obj.may,
+                obj.jun,
+                obj.jul,
+                obj.aug,
+                obj.sep,
+                obj.oct,
+                obj.nov,
+                obj.dec,
+                obj.jan,
+                obj.feb,
+                obj.mar,
+            ]
+        except :
+            # TODO add error handling to catch the no data situation.
+            pass
 
 
 def create_oscar_report():
