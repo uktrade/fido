a85c4b7ee2076d3b233b4803de4a64fff67aa5a4
Luisella Strona <luisella21@hotmail.com>
2020-04-03 09:00:45 +0100

    Created the end of month action.

--- end_of_month/end_of_month_actions.py
+++ end_of_month/end_of_month_actions.py
@@ -1,5 +1,6 @@
 from django.db import (
     DatabaseError,
+    connection,
     transaction,
 )
 from django.db.models import F, Value, IntegerField
@@ -13,39 +14,48 @@ from forecast.models import (BudgetMonthlyFigure,
 
 from simple_history.utils import bulk_create_with_history
 
-@transaction.atomic
+def insert_query(table_name, archived_period):
+    return f'INSERT INTO public.{table_name}(' \
+               f'created, updated, amount, starting_amount, financial_code_id, ' \
+               f'financial_period_id, financial_year_id) ' \
+               f'SELECT ' \
+               f'created, updated, amount, amount, financial_code_id, ' \
+               f'financial_period_id, financial_year_id ' \
+               f'FROM public.{table_name} ' \
+               f'WHERE archived_status_id = {archived_period.id}'
+
+
+# TODO error handling
 def end_of_month_archive(end_of_month_info):
-    try:
-        with transaction.atomic():
-            period_id = end_of_month_info.archived_period.financial_period_code
-            # Use transaction
-            # Add archive period to all the active forecast
-            archived_periods = ForecastMonthlyFigure.objects.filter(
-                financial_period__financial_period_code__gt=period_id,
-                archived_status__isnull=True)
-            archived_periods.update(archived_status=end_of_month_info)
+    period_id = end_of_month_info.archived_period.financial_period_code
+    # I cannot use transactions because of the direct SQL
+    # Add archive period to all the active forecast
+    forecast_periods = ForecastMonthlyFigure.objects.filter(
+        financial_period__financial_period_code__gt=period_id,
+        archived_status__isnull=True)
+    forecast_periods.update(archived_status=end_of_month_info)
 
-            # Copy forecast just archived to current forecast
-            # current_forecast = ForecastMonthlyFigure.objects.filter(archived_status=end_of_month_info)\
-            #     .values('financial_year',
-            #             'financial_period',
-            #             'financial_code',
-            #             'amount',
-            # ).annotate(starting_amount=F('amount'),
-            #            archived_status=Value(None, output_field=IntegerField()))
+    # Copy forecast just archived to current forecast
+    # Use direct SQL for performance reason.
+    # Simple history does not get updated, but it recovers
+    #  when changes are done to the record.
+    # Note that initial amount is updated to be the current amount.
+    forecast_sql = insert_query('forecast_forecastmonthlyfigure',
+                                end_of_month_info.archived_period.id)
+    with connection.cursor() as cursor:
+        cursor.execute(forecast_sql)
 
-            current_forecast = ForecastMonthlyFigure.objects.filter(archived_status=end_of_month_info)\
-                .values('financial_year',
-                        'financial_period',
-                        'financial_code',
-                        'amount',
-            ).annotate(starting_amount=F('amount'), pk = 100000+F'pk')
+    # Archive the budget
+    budget_periods = BudgetMonthlyFigure.objects.filter(
+        financial_period__financial_period_code__gt=period_id,
+        archived_status__isnull=True)
+    budget_periods.update(archived_status=end_of_month_info)
+    budget_sql =insert_query('forecast_budgetmonthlyfigure',
+                             end_of_month_info.archived_period.id)
 
-            # models.TestModel.objects.bulk_create(instances)
-            objs = bulk_create_with_history(current_forecast, ForecastMonthlyFigure, batch_size=500)
-            # Refresh the amount to  the start of month field
+    with connection.cursor() as cursor:
+        cursor.execute(budget_sql)
 
-            # Archive the budget
-            # set the archived status to archived
-    except DatabaseError:
-        obj.active = False
+    # set the archived status to archived
+    end_of_month_info.archived = True
+    end_of_month_info.save()
