c05e686ebd625add1293a72602cb1e86747372ce
Ross Miller <ross@decodedcollective.com>
2020-04-09 15:15:35 +0100

    Update gitignore, amend requirements

--- core/management/commands/elevate_sso_user_permissions.py
+++ core/management/commands/elevate_sso_user_permissions.py
@@ -7,14 +7,29 @@ from django.db.models import Q
 class Command(BaseCommand):
     help = "Elevate SSO user permissions for local development purposes"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--email",
+            help="Test user's email address",
+            dest="email",
+        )
+
     def handle(self, *args, **options):
         if settings.CAN_ELEVATE_SSO_USER_PERMISSIONS:
+            email = options["email"]
+
             user = get_user_model()
-            sso_user = user.objects.exclude(
-                email="AnonymousUser",
-            ).exclude(
-                Q(email__contains='test.com')
-            ).first()
+
+            if email:
+                sso_user = user.objects.get(
+                    email=email,
+                )
+            else:
+                sso_user = user.objects.exclude(
+                    email="AnonymousUser",
+                ).exclude(
+                    Q(email__contains='test.com')
+                ).first()
             sso_user.is_superuser = True
             sso_user.is_staff = True
             sso_user.save()
