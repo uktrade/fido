eebbf955306d1ef85d8bb8c8e4663ea9def6afce
Ross Miller <ross@decodedcollective.com>
2020-04-09 15:46:43 +0100

    Update procfile to use compilescss, debug whitenoise

--- config/settings/base.py
+++ config/settings/base.py
@@ -247,13 +247,13 @@ SETTINGS_EXPORT = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "core.no_cache_middleware.NoCacheMiddleware",
     "simple_history.middleware.HistoryRequestMiddleware",
     "axes.middleware.AxesMiddleware",
