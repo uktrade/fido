90b4c0d055de5e83e60c11ae5c08150205327ef9
Luisella Strona <luisella21@hotmail.com>
2020-04-21 07:40:31 +0100

    Safety commit

--- chartofaccountDIT/test/test_archive.py
+++ chartofaccountDIT/test/test_archive.py
@@ -9,34 +9,45 @@ from django.urls import reverse
 from core.myutils import get_current_financial_year
 from core.test.test_base import RequestFactoryBase
 
-from chartofaccountDIT.test.factories import (
-    Analysis1Factory,
-    Analysis2Factory,
+from costcentre.test.factories import (
+    CostCentreFactory,
+    DepartmentalGroupFactory,
+    DirectorateFactory,
 )
-
-from chartofaccountDIT.views import (
-    HistoricalFilteredAnalysis1ListView,
-    HistoricalFilteredAnalysis2ListView,
-)
+from costcentre.views import HistoricalFilteredCostListView
 
 
-class ArchiveAnalysis1Test(TestCase, RequestFactoryBase):
+class ArchiveCostCentreTest(TestCase, RequestFactoryBase):
     def setUp(self):
         self.out = StringIO()
         RequestFactoryBase.__init__(self)
 
-        analysis1 = Analysis1Factory()
-        self.analysis_code = analysis1.analysis1_code
-        self.analysis1_description = analysis1.analysis1_description
+        self.group_name = "Test Group"
+        self.group_code = "TestGG"
+        self.directorate_name = "Test Directorate"
+        self.directorate_code = "TestDD"
+        self.cost_centre_code = 109076
+
+        group = DepartmentalGroupFactory(
+            group_code=self.group_code, group_name=self.group_name,
+        )
+        directorate = DirectorateFactory(
+            directorate_code=self.directorate_code,
+            directorate_name=self.directorate_name,
+            group=group,
+        )
+        CostCentreFactory(
+            directorate=directorate, cost_centre_code=self.cost_centre_code,
+        )
         current_year = get_current_financial_year()
         self.archive_year = current_year - 1
 
     def show_historical_view(self):
         response = self.factory_get(
             reverse(
-                "historicalanalysis1", kwargs={"year": self.archive_year},
+                "historical_cost_centre_filter", kwargs={"year": self.archive_year},
             ),
-            HistoricalFilteredAnalysis1ListView,
+            HistoricalFilteredCostListView,
             year=self.archive_year,
         )
         self.assertEqual(response.status_code, 200)
@@ -54,7 +65,7 @@ class ArchiveAnalysis1Test(TestCase, RequestFactoryBase):
         assert len(table_rows) == 2
 
         call_command(
-            "archive", type="Analysis1", year=self.archive_year, stdout=self.out,
+            "archive", type="CostCentre", year=self.archive_year, stdout=self.out,
         )
         soup = self.show_historical_view()
         table_rows = soup.find_all("tr", class_="govuk-table__row")
@@ -65,3 +76,6 @@ class ArchiveAnalysis1Test(TestCase, RequestFactoryBase):
         first_cols = table_rows[0].find_all("td")
         assert first_cols[0].get_text().strip() == self.group_code
         assert first_cols[1].get_text().strip() == self.group_name
+        assert first_cols[2].get_text().strip() == self.directorate_code
+        assert first_cols[3].get_text().strip() == self.directorate_name
+        assert first_cols[4].get_text().strip() == str(self.cost_centre_code)
