c7094db63596f3d2a42992197833ec6827303449
Ross Miller <github@decodedcollective.com>
2020-07-16 12:47:49 +0100

    Debug order of play issue

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -1,6 +1,8 @@
 from psycogreen.gevent import patch_psycopg
+#from gevent import monkey
 
 
 def post_fork(server, worker):
+    #monkey.patch_all()
     patch_psycopg()
     worker.log.info("Enabled async Psycopg2")
