13e54bb7b567e653b80e980476c213bd95e19f83
nboyse <natasha.boyse@digital.trade.gov.uk>
2020-09-04 15:53:51 +0100

    tests

--- core/test/test_log_entry.py
+++ core/test/test_log_entry.py
@@ -1,2 +1,18 @@
-def test_download_return_column():
+import io
+
+from django.contrib.auth.models import Permission
+from django.test import TestCase
+from django.urls import reverse
+
+from openpyxl import load_workbook
+from core.test.test_base import RequestFactoryBase
+
+
+class DownloadMIReportTest(TestCase, RequestFactoryBase):
+    def setUp(self):
+        RequestFactoryBase.__init__(self)
+
+    def test_download(self):
+
+
 
