c744bd2817b6f80f5bbdfe39e1612825423c3e46
Tash Boyse <57753415+nboyse@users.noreply.github.com>
2020-09-16 13:32:20 +0100

    Unit tests for Cost Centre Hierarchy

--- costcentre/test/factories.py
+++ costcentre/test/factories.py
@@ -2,8 +2,12 @@ import factory
 
 from faker import Faker
 
+from core.models import FinancialYear
+
 from costcentre.models import (
     ArchivedCostCentre,
+    BSCEEmail,
+    BusinessPartner,
     CostCentre,
     DepartmentalGroup,
     Directorate,
@@ -38,6 +42,34 @@ class DirectorateFactory(factory.DjangoModelFactory):
     active = True
 
 
+class FinancialYearFactory(factory.DjangoModelFactory):
+
+    class Meta:
+        model = FinancialYear
+        django_get_or_create = ('financial_year',)
+
+    financial_year = 2019
+
+
+class FinanceBusinessPartnerFactory(factory.DjangoModelFactory):
+
+    class Meta:
+        model = BusinessPartner
+        django_get_or_create = ('name', 'surname')
+
+    name = "test"
+    surname = "FBP"
+
+
+class BSCEFactory(factory.DjangoModelFactory):
+
+    class Meta:
+        model = BSCEEmail
+        django_get_or_create = ('bsce_email',)
+
+    bsce_email = "bsceuser@test.com"
+
+
 class CostCentreFactory(factory.DjangoModelFactory):
     """
         Define CostCentre Factory
@@ -51,6 +83,8 @@ class CostCentreFactory(factory.DjangoModelFactory):
     directorate = factory.SubFactory(DirectorateFactory)
     cost_centre_code = 999999
     cost_centre_name = "Test Cost Centre"
+    business_partner = factory.SubFactory(FinanceBusinessPartnerFactory)
+    bsce_email = factory.SubFactory(BSCEFactory)
 
 
 class ArchivedCostCentreFactory(factory.DjangoModelFactory):
